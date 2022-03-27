5924b094037186b7ce92b6dd235f1f87a5de9006
Даниил Рудь <ruddanil99@gmail.com>
2022-03-27 19:56:35 +0400

    Creation DataLayer for Users

---
+++ DataLayer/SQLDataHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class SQLDataHelper
+    {
+        protected string DataBaseConnectionPath { get; set; }
+
+        public SQLDataHelper(string nameTable, string connectionPath )
+        {
+            NameTable = nameTable;
+            Table = new DataTable();
+        }
+
+        public string NameTable { get; private set; }
+        protected DataTable Table { get; set; }
+
+        public new DataTable CustomSql(string request)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection connection = new SqlConnection(DataBaseConnectionPath))
+            using (SqlCommand cmd = new SqlCommand(request, connection))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(request, connection);
+                adapter.Fill(table);
+                cmd.ExecuteNonQuery();
+            }
+            return table;
+        }
+
+    }
+}
